0a7f688d554eeb63e04d376326c018009edd85d5
asdasdasdasdarsen <arsenii.tesliuk@gmail.com>
2025-04-05 17:38:25 +0300

    lesson 10

---
+++ lesson 10.py
@@ -0,0 +1,56 @@
+# from bs4 import BeautifulSoup
+# import requests
+# response = requests.get("https://coinmarketcap.com/")
+# if response.status_code == 200:
+#     soup = BeautifulSoup(response.text,features="html.parser")
+#     soup_list = soup.find_all("a", {"href": "/currencies/bitcoin/#markets"})
+#     res = soup_list[0].find("span")
+#     print(res)
+#
+
+
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://uaserials.pro/films/"
+
+r = requests.get(url)
+soup = BeautifulSoup(r.text, features="html.parser")
+soup_list_name = soup.find_all('div', {"class": "th-title truncate"})
+
+name_list = []
+for i in soup_list_name:
+    name_list.appand(i.text)
+
+f = open('film.txt','w',encoding='utf-8')
+for film in name_list:
+    f.write(f" {film}\n")
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
